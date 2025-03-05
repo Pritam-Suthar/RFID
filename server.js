5c445c761cc11723bc54c5cf044fe54a8530c4d8
Pritam-Suthar <pritam.suthar@aigonity.com>
2025-03-05 18:25:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const paymentRoutes = require("./routes/paymentRoutes");
+const rfidRoutes = require("./routes/rfidRoutes");
 
 const app = express();
 app.use(cors());
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 
 // Use payment routes
 app.use("/api/payments", paymentRoutes);
+app.use("/api/rfid", rfidRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
